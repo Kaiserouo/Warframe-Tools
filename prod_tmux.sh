2e229e80bff14f921cc6533fee544b3d24ddba11
Kaiserouo <chejps30221@gmail.com>
2026-04-11 10:37:08 +0800

    add prod tmux

---
+++ prod_tmux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sets up the development environment in tmux
+# specifically, react-vite on the left and python backend server on the right
+
+# please make sure that you use LF instead of CRLF in VSCode setting
+
+tmux new-session -d -s wf;
+tmux send-keys -t wf 'git pull; cd src/web/frontend; npm build; cd ../../..; python -m src.web.backend.server' C-m;
+
+tmux attach -t wf;
